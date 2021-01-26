48bba86d010ec15e39a32bd61b9369cc5316f1ac
SebastKl <sebastianklaus962@gmail.com>
2021-01-26 13:10:02 +0100

    2 menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './modules/menu/menu.component';
+import { HeaderComponent } from './modules/header/header.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Menu2Component } from './modules/menu2/menu2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    Menu2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
